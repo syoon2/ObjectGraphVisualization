f48714b195ed205b78670309f5239c6fe46b8601
syoon2 <syoon2@mta.ca>
2023-10-23 14:25:22 +0900

    Get rid of the second main method
    There really should be only one.

--- src/main/java/ch/hsr/ogv/Main.java
+++ src/main/java/ch/hsr/ogv/Main.java
@@ -1,12 +1,27 @@
 package ch.hsr.ogv;
 
+import java.util.Locale;
+
+import javafx.application.Application;
+import javafx.scene.text.Font;
+
+import org.apache.logging.log4j.LogManager;
+
+import ch.hsr.ogv.util.ResourceLocator;
+
 /**
  * Starts the application.
  */
 public class Main {
 
     public static void main(String[] args) {
-        MainApp.main(args);
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            LogManager.getLogger(MainApp.class).catching(throwable);
+        });
+        Locale.setDefault(new Locale("en", "EN")); // set to English
+        Font.loadFont(ResourceLocator.getResourcePath(ResourceLocator.Resource.SEGOEUI_TTF).toExternalForm(), Font.getDefault().getSize());
+        Font.loadFont(ResourceLocator.getResourcePath(ResourceLocator.Resource.LUCIDASANS_TTF).toExternalForm(), Font.getDefault().getSize());
+        Application.launch(MainApp.class, args);
     }
 
 }
--- src/main/java/ch/hsr/ogv/MainApp.java
+++ src/main/java/ch/hsr/ogv/MainApp.java
@@ -3,20 +3,14 @@ package ch.hsr.ogv;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
-import java.lang.Thread.UncaughtExceptionHandler;
-import java.util.Locale;
 import java.util.Properties;
 
 import javafx.application.Application;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import ch.hsr.ogv.util.ResourceLocator;
-import ch.hsr.ogv.util.ResourceLocator.Resource;
 
 /**
  * Starts the FX application.
@@ -35,20 +29,9 @@ public class MainApp extends Application {
 
     private final static Logger logger = LogManager.getLogger(MainApp.class);
 
-    private final static UncaughtExceptionHandler ueHandler = (thread, throwable) -> {
+    private final static Thread.UncaughtExceptionHandler ueHandler = (thread, throwable) -> {
         logger.catching(Level.DEBUG ,throwable);
     };
-
-    public static void main(String[] args) {
-        // System.setProperty("prism.lcdtext", "false");
-        // System.setProperty("prism.text", "t2k");
-        // System.setProperty("prism.marlin", "true");
-        Locale.setDefault(new Locale("en", "EN")); // set to English
-        Font.loadFont(ResourceLocator.getResourcePath(Resource.SEGOEUI_TTF).toExternalForm(), Font.getDefault().getSize());
-        Font.loadFont(ResourceLocator.getResourcePath(Resource.LUCIDASANS_TTF).toExternalForm(), Font.getDefault().getSize());
-        Thread.setDefaultUncaughtExceptionHandler(ueHandler);
-        launch(args);
-    }
 
     @Override
     public void start(Stage primaryStage) {
